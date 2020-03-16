18a5f538c158e09a22993a2463394a29e14684ff
VitorFellix <vitor_fellix@hotmail.com>
2020-03-15 22:32:25 -0300

    Ajustes
    Apenas alguns ajustes

--- src/main/java/projeto/Main.java
+++ src/main/java/projeto/Main.java
@@ -1,11 +1,9 @@
 package projeto;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
